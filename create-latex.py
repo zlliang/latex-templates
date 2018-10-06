78fa8609122a4a133f102dfc49e676d021b88969
Zilong Liang <zlliang15@fudan.edu.cn>
2018-10-06 10:53:35 +0800

    Add create-latex.py

---
+++ create-latex.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Create LaTeX documents."""
+
+import argparse
+import os
+
+parser = argparse.ArgumentParser(
+    prog="create-latex",
+    description="Create LaTeX documents."
+)
+
+parser.add_argument(
+    "docname",
+    metavar="name",
+    help="The name of the document to create."
+)
+
+parser.add_argument(
+    "-n", "--new",
+    help="Create as a new project",
+    action="store_true"
+)
+
+args = parser.parse_args()
+
+os.system("cp ~/Documents/dev/texmoon/document.tex {}.tex".format(args.docname))
+if args.new:
+    os.system("cp ~/Documents/dev/texmoon/preamble.tex .")
